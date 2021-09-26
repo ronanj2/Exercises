b487f352a7f07a31d79bb12b4995562054a4ae71
Ronan Farrell <ronan.farrell@j2.com>
2021-09-26 21:11:58 +0100

    Exercise 4 Part 1

---
+++ src/com/sw_dev/inheritance/exercises/exercise4_1/Student.java
@@ -0,0 +1,15 @@
+package com.sw_dev.inheritance.exercises.exercise4_1;
+
+public class Student extends Person {
+    private String degree;
+
+    public Student(String name, int age, String degree) {
+        super(name, age);
+        this.degree = degree;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s who is an student studying %s", super.toString(), this.degree);
+    }
+}
